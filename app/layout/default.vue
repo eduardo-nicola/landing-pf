b29560e2a6fcf07309f2698d0e79132d733b4c4d
eduardo-nicola <eduardonicolagit@gmail.com>
2025-08-22 15:06:52 -0300

    init nuxt

---
+++ app/layout/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main-layout">
+    <h1> titulo </h1>
+    <slot />
+  </main>
+</template>
+<style>
+.main-layout {
+  min-height: 100vh;
+  min-width: 100vw;
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Eduardo 2</h1>
+</template>
