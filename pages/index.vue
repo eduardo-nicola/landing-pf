e7fb3fc5ade0044b0ed6ac3383bfcfbac5b752c7
eduardo-nicola <eduardonicolagit@gmail.com>
2025-08-22 14:27:23 -0300

    reset

--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold mb-4">Welcome to My Nuxt Page</h1>
-    <p class="mb-4">This is a simple page using Nuxt.js and Tailwind CSS.</p>
-    <nuxt-link to="/about" class="text-blue-500 hover:underline">Go to About Page</nuxt-link>
-  </div>
-</template>
